d9a844935eed5fdd528a49bb7cfcc76a51078c85
Arsennnic <arsennnic@gmail.com>
2020-01-07 12:35:57 +0800

    Ignore bin/ and src/ directories

--- analytic.f90
+++ analytic.f90
@@ -3,23 +3,29 @@ subroutine analytic
     use params
     implicit none
 
+    real*8, parameter      :: PI = 4*atan(1.0_8)
+    integer*4              :: s, smax
+    real*8, allocatable    :: A2s(:), B2s(:)
+    real*8, allocatable    :: A3s(:), B3s(:)
+    real*8, dimension(N,N) :: X2d, Y2d
+    real*8, dimension(N,N) :: theta1, theta2, theta3
+    
     character(len=:), allocatable :: dir
-    real(kind=8) :: pi = 4*atan(1.0_8)
-    integer(kind=4) :: s, smax
-    real(kind=8), dimension(:), allocatable :: A2s, B2s, A3s, B3s
-    real(kind=8), dimension(N,N) :: X2d, Y2d, theta1, theta2, theta3
-    integer(kind=4) :: I, J, P
+    integer*4                     :: I, J, P
 
     call init()
+
+    dir  = ".\\out\\analytic\\"
+    call checkdir(dir)
 
     smax = 100
     allocate(A2s(smax), B2s(smax), A3s(smax), B3s(smax))
     do s = 1, smax
         if (mod(s, 2) == 1) then
-            A2s(s) = -2*H*qw/lambda/(T1- T0)/(s*pi)**2
-            B2s(s) = -A2s(s)/tanh(s*pi)
-            A3s(s) = -2*H*qe/lambda/(T1- T0)/(s*pi)**2
-            B3s(s) = -A3s(s)/tanh(s*pi)
+            A2s(s) = -2*H*qw/lambda/(T1- T0)/(s*PI)**2
+            B2s(s) = -A2s(s)/tanh(s*PI)
+            A3s(s) = -2*H*qe/lambda/(T1- T0)/(s*PI)**2
+            B3s(s) = -A3s(s)/tanh(s*PI)
         else
             A2s(s) = 0
             B2s(s) = 0
@@ -34,14 +40,15 @@ subroutine analytic
     theta2 = 0
     theta3 = 0
     do s = 1, smax
-        theta2 = theta2 + (A2s(s)*sinh(s*pi*X2d) + B2s(s)*cosh(s*pi*X2d))*sin(s*pi*Y2d)
-        theta3 = theta3 + (A3s(s)*sinh(s*pi*(1 - X2d)) + B3s(s)*cosh(s*pi*(1 - X2d)))*sin(s*pi*Y2d)
+        theta2 = theta2 + (A2s(s)*sinh(s*PI*X2d) + B2s(s)*cosh(s*PI*X2d))*sin(s*PI*Y2d)
+        theta3 = theta3 + (A3s(s)*sinh(s*PI*(1 - X2d)) + B3s(s)*cosh(s*PI*(1 - X2d)))*sin(s*PI*Y2d)
     end do
 
     theta = reshape(theta1 + theta2 + theta3, (/N*N/))
     T = theta*(T1 - T0) + T0
-    dir = ".\\out\\analytic\\"
     call visualize(dir, 0)
+
+    ! call output(dir, 0, "Temperature", T)
 
     deallocate(x, y, t, theta)
 
---
+++ output.f90
@@ -0,0 +1,69 @@
+! =================================================
+!   Subroutines to output data files, for post- 
+!   processing.
+! 
+!   Usage: 
+!   1. Check if output directory exists. Add the 
+!      following lines outside the time-marching 
+!      loop.
+! 
+!      character(len=:), allocatable :: dir
+!      integer*4                     :: iter
+!      iter = 0
+!      dir  = ".\\out\\"
+!      call checkdir(dir)
+! 
+!   2. Call subroutine to write data files. Add 
+!      the following lines in the time-marching 
+!      loop, note that "name" and "value" should 
+!      be replaced with specific name and value
+!      of your variale.
+!
+!      iter = iter + 1
+!      call output(dir, iter, "name", vaule)
+! 
+! =================================================
+
+
+subroutine checkdir(dir)
+
+    implicit none
+
+    character(len=*) :: dir
+    logical :: dir_exist
+
+    inquire(file=dir, exist=dir_exist)
+    if (dir_exist) then
+        call system("rmdir /s/q "//dir)
+    end if
+    call system("md "//dir)
+
+end subroutine
+
+
+subroutine output(dir, iter, name, value)
+
+    use params, only: Nx, Ny
+    implicit none
+
+    character(len=*), intent(in) :: dir, name
+    integer*4,        intent(in) :: iter
+    real*8,           intent(in) :: value(Nx,Ny)
+    character(len=8)             :: iter_str
+    character(len=:)             :: file_name
+    integer*4                    :: I, J
+
+    write(iter_str, '(I8.8)') iter
+    file_name = dir//name//"."//iter_str//".dat"
+    open(10, file=file_name, action="write", &
+        & status="replace")
+    write(10, *) "variables=x,y,"//name
+    write(10, *) "zone i=", Nx, "j=", Ny, "f=point"
+    do J = 1, Ny
+        do I = 1, Nx
+            write(10, *) I, J, value(I,J)
+        end do
+    end do
+    close(10)
+
+end subroutine
